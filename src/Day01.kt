6c5882476750b2e04dbfe6cc2e36f5ba1eae0edc
jorge castrejon <7059764+jorgecastrejon@users.noreply.github.com>
2024-12-01 09:19:55 +0100

    adding solution to day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,41 @@
+import java.util.*
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val left = PriorityQueue<Int>()
+        val right = PriorityQueue<Int>()
+
+        input.forEach { line ->
+            val (a, b) = line.split("   ")
+            left.add(a.toInt())
+            right.add(b.toInt())
+        }
+
+        var totalDistance  = 0
+
+        while (!left.isEmpty()) {
+            totalDistance += abs(left.remove() - right.remove())
+        }
+
+        return totalDistance
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val left = mutableListOf<Int>()
+        val right = mutableMapOf<Int, Int>()
+
+        input.forEach { line ->
+            val (a, rawB) = line.split("   ")
+            left.add(a.toInt())
+            val b = rawB.toInt()
+            right[b] = right.getOrDefault(b, 0) + 1
+        }
+
+        return left.sumOf { number -> number * right.getOrDefault(number,0) }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
